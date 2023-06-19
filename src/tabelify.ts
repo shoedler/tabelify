ab08771108e4a38e197e6534845b8bd60d0d1a21
Simon Schödler <simon.schoedler@hotmail.ch>
2023-06-19 22:41:49 +0200

    Refactors

--- src/tabelify.ts
+++ src/tabelify.ts
@@ -14,73 +14,77 @@ const stripAnsi = (text: string) => text.replace(ansiRegex, '');
 type ColumnOptions<T> = {
   [k in keyof Partial<T>]: {
     titleOverride?: string;
-    alignment?: 'left' | 'right' | 'center';
+    horizontalAlignment?: `${'left' | 'right' | 'center'}`;
+    verticalAlignment?: `${'top' | 'bottom' | 'center'}`;
     formatter?: (value: T[k]) => string;
     showDiffToPrevious?: boolean;
   };
 };
 
 type TabelifyOptions = {
-  roundTo?: 1 | 0.1 | 0.01 | 0.001 | 0.0001 | 0.00001;
+  rowDivider?: true;
 };
 
-export function tabelify<T>(
+type Cell<T> = {
+  column: keyof T;
+  content: string[];
+  width: number;
+  options: ColumnOptions<T>[keyof T];
+};
+
+export function tabelify<T, K extends keyof T>(
   data: T[],
-  selector: (keyof T)[],
+  selector: K[],
   config?: {
-    previousData?: T[];
-    columnOptions?: ColumnOptions<T>;
+    columnOptions?: ColumnOptions<Pick<T, K>>;
     tabelifyOptions?: TabelifyOptions;
   },
 ): string {
-  const { previousData, columnOptions } = config || {};
+  const { columnOptions, tabelifyOptions } = config || {};
 
-  //
-  // Create data table from selector and mapped data
-  const headerData: string[] = selector.map((key) => {
-    const title =
-      columnOptions && columnOptions[key] && columnOptions[key].titleOverride
-        ? columnOptions[key].titleOverride
-        : key.toString();
-    return c.bold(title);
+  // Build header
+  const headerData: Cell<T>[] = selector.map((key) => {
+    const options = columnOptions && columnOptions[key] ? columnOptions[key] : {};
+
+    const title = options.titleOverride ? options.titleOverride : key.toString();
+
+    const column = key;
+    const content = c.bold(title).split('\n');
+    const width = Math.max(...content.map((line) => stripAnsi(line).length));
+
+    return {
+      column,
+      content,
+      width,
+      options,
+    };
   });
 
-  const tableData: string[][] = data.map((item, i) =>
+  // Build table
+  const tableData: Cell<T>[][] = data.map((item) =>
     selector.map((key) => {
       const value = item[key];
+      const options = columnOptions && columnOptions[key] ? columnOptions[key] : {};
+      const formatter = options.formatter ? options.formatter : styleCell;
 
-      const previousValue = previousData && previousData.length > 0 ? previousData[i][key] : undefined;
+      const cell = formatter(value);
 
-      const showDiffToPrevious = columnOptions && columnOptions[key] && columnOptions[key].showDiffToPrevious;
-      const addDiffRight = columnOptions && columnOptions[key] && columnOptions[key].alignment === 'right';
-      const formatter =
-        columnOptions && columnOptions[key] && columnOptions[key].formatter ? columnOptions[key].formatter : styleCell;
+      const column = key;
+      const content = cell.split('\n');
+      const width = Math.max(...content.map((line) => stripAnsi(line).length));
 
-      if (showDiffToPrevious && previousValue !== undefined) {
-        if (typeof value === 'number') {
-          let diff = value - (previousValue as typeof value);
-          if (diff === 0) {
-            return formatter(value);
-          }
-          diff = Math.round(diff * 10) / 10;
-          return addDiffRight ? `${formatter(value)} (${c.black(diff)})` : `(${c.black(diff)}) ${formatter(value)}`;
-        }
-        if (typeof value === 'string') {
-          const diff = previousValue as typeof value;
-          return `${formatter(value)} (${c.black('was ' + diff)})`;
-        } else {
-          return `${formatter(value)} (${c.black('changed')})`;
-        }
-      }
-
-      return formatter(value);
+      return {
+        column,
+        content,
+        width,
+        options,
+      };
     }),
   );
 
   const table = [headerData, ...tableData];
 
-  //
-  // Find the longest cell in each column
+  // Calculate column widths and row heights
   const columnWidths = new Array(selector.length).fill(0);
   const rowHeights = new Array(table.length).fill(0);
 
@@ -88,63 +92,80 @@ export function tabelify<T>(
     const row = table[i];
     for (let j = 0; j < row.length; j++) {
       const cell = row[j];
-      const strippedCell = stripAnsi(cell);
-      columnWidths[j] = Math.max(columnWidths[j], strippedCell.length);
-      rowHeights[i] = Math.max(rowHeights[i], cell.split('\n').length);
+
+      columnWidths[j] = Math.max(columnWidths[j], cell.width);
+      rowHeights[i] = Math.max(rowHeights[i], cell.content.length);
     }
   }
 
-  //
-  // Normalize table by padding each cell with spaces
-  const normalizedTable = table.map((row) => {
-    return row.map((cell, i) => {
-      const strippedCell = stripAnsi(cell);
-      let padAmount = columnWidths[i] - strippedCell.length;
+  // Normalize table by padding each cell with spaces and newlines
+  for (let i = 0; i < table.length; i++) {
+    const row = table[i];
+    for (let j = 0; j < row.length; j++) {
+      const cell = row[j];
+      const cellPad = columnWidths[j];
+      const verticalAlignment = cell.options.verticalAlignment ?? 'center';
+      const horizontalAlignment = cell.options.horizontalAlignment ?? 'center';
 
-      // For each umlaut (diacritic), add one more space. Currently, only U+0308 is supported. (7̈ )
-      const umlauts = strippedCell.match(/\u{308}/gmu);
-      if (umlauts) {
-        padAmount += umlauts.length;
+      // Pad cell horizontally
+      for (let k = 0; k < cell.content.length; k++) {
+        const line = cell.content[k];
+        const strippedLine = stripAnsi(line);
+        const padAmount = cellPad - strippedLine.length + (strippedLine.match(/\u{308}/gmu)?.length ?? 0); // For each umlaut (diacritic), add one more space. Currently, only U+0308 is supported. (7̈ )
+
+        if (horizontalAlignment === 'right') {
+          cell.content[k] = ' '.repeat(padAmount) + line;
+        } else if (horizontalAlignment === 'left') {
+          cell.content[k] = line + ' '.repeat(padAmount);
+        } else {
+          const leftPad = Math.floor(padAmount / 2);
+          const rightPad = Math.ceil(padAmount / 2);
+
+          cell.content[k] = ' '.repeat(leftPad) + line + ' '.repeat(rightPad);
+        }
       }
 
-      const alignment =
-        columnOptions && columnOptions[selector[i]] && columnOptions[selector[i]].alignment
-          ? columnOptions[selector[i]].alignment
-          : 'left';
+      // Pad cell vertically
+      if (verticalAlignment === 'top' || verticalAlignment === 'bottom') {
+        const emptyLines = new Array(rowHeights[i] - cell.content.length).fill(' '.repeat(cellPad));
 
-      let padded = '';
-
-      if (alignment === 'right') {
-        padded = ' '.repeat(padAmount) + cell;
-      } else if (alignment === 'left') {
-        padded = cell + ' '.repeat(padAmount);
+        cell.content =
+          verticalAlignment === 'top' ? [...cell.content, ...emptyLines] : [...emptyLines, ...cell.content];
       } else {
-        // Center
-        const leftPad = Math.floor(padAmount / 2);
-        const rightPad = Math.ceil(padAmount / 2);
+        const topPad = Math.floor((rowHeights[i] - cell.content.length) / 2);
+        const bottomPad = Math.ceil((rowHeights[i] - cell.content.length) / 2);
 
-        padded = ' '.repeat(leftPad) + cell + ' '.repeat(rightPad);
+        cell.content = [
+          ...new Array(topPad).fill(' '.repeat(cellPad)),
+          ...cell.content,
+          ...new Array(bottomPad).fill(' '.repeat(cellPad)),
+        ];
       }
+    }
+  }
 
-      padded += ' '.repeat(rowHeights[i] - cell.split('\n').length);
-
-      return padded;
-    });
-  });
-
-  //
   // Borders
   const horizontalBorder = columnWidths.map((width) => '-'.repeat(width));
 
   let tableString = '+-' + horizontalBorder.join('-+-') + '-+\n';
-  tableString += '| ' + normalizedTable[0].join(' | ') + ' |\n';
+  for (let i = 0; i < rowHeights[0]; i++) {
+    tableString += '| ' + table[0].map((cell) => cell.content[i]).join(' | ') + ' |\n';
+  }
   tableString += '+-' + horizontalBorder.join('-+-') + '-+\n';
 
-  for (let i = 1; i < normalizedTable.length; i++) {
-    tableString += '| ' + normalizedTable[i].join(' | ') + ' |\n';
+  for (let i = 1; i < table.length; i++) {
+    for (let j = 0; j < rowHeights[i]; j++) {
+      tableString += '| ' + table[i].map((cell) => cell.content[j]).join(' | ') + ' |\n';
+    }
+
+    if (tabelifyOptions?.rowDivider) {
+      tableString += '+-' + horizontalBorder.join('-+-') + '-+\n';
+    }
   }
 
-  tableString += '+-' + horizontalBorder.join('-+-') + '-+\n';
+  if (!tabelifyOptions?.rowDivider) {
+    tableString += '+-' + horizontalBorder.join('-+-') + '-+\n';
+  }
 
   return tableString;
 }
@@ -168,12 +189,12 @@ const styleCell = (value: any): string => {
 };
 
 const sampleData = [
-  { name: 'John', age: 24, city: 'New York', country: 'USA' },
+  { name: 'John', age: 24, city: 'New York\nYay!', country: 'USA' },
   { name: 'Jane', age: 18, city: 'London', country: 'UK' },
   { name: 'Bob', age: 32, city: 'Paris', country: 'France' },
-  { name: 'Mary', age: 27, city: 'Berlin', country: 'Germany' },
-  { name: 'Mike', age: 41, city: 'Rome', country: 'Italy' },
-  { name: 'Lisa', age: 19, city: 'Madrid', country: 'Spain' },
+  { name: 'Mary', age: 27, city: 'Berlin', country: [0] },
+  { name: 'Mike', age: 41, city: 'Rome', country: {} },
+  { name: 'Lisa\nIs\nMy\nNaaaame!!!', age: 19, city: 'Madrid', country: 'Spain' },
   { name: 'Tom', age: 23, city: 'Vienna', country: 'Austria' },
   { name: 'Tim', age: 35, city: 'Athens', country: 'Greece' },
   { name: 'Kim', age: 29, city: 'Amsterdam', country: 'Netherlands' },
@@ -186,12 +207,24 @@ const sampleData = [
 
 const out = tabelify(sampleData, ['name', 'age', 'city', 'country'], {
   columnOptions: {
+    name: {
+      titleOverride: c.blueBright('Name'),
+    },
     age: {
-      alignment: 'right',
+      titleOverride: c.blueBright('Age'),
+      horizontalAlignment: 'right',
     },
     country: {
-      alignment: 'center',
+      titleOverride: c.blueBright('Country'),
+      horizontalAlignment: 'center',
     },
+    city: {
+      titleOverride: c.blueBright('City'),
+      horizontalAlignment: 'left',
+    },
+  },
+  tabelifyOptions: {
+    rowDivider: true,
   },
 });
 
